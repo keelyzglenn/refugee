c5a12166ebd86897a160d1aeb6c0ef8670e0b7af
Cassie Musolf <cassiemusolf@gmail.com>
2017-04-11 14:37:07 -0700

    add more information about the healthcare section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Angular2FontAwesomeModule]
 })
 export class AppComponent {
 }
