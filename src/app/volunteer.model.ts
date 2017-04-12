bf2b746a0502d767d41351a22a999284812ac374
Jiwon Kang and Tammy Dang <jiwon+tammy@>
2017-04-12 10:51:08 -0700

    add styling to volunteer section

--- src/app/volunteer.model.ts
+++ src/app/volunteer.model.ts
@@ -1,3 +1,3 @@
 export class Volunteer {
-  constructor (public company: string, public location: string, public link: string, public summary: string, public category: string) { }
+  constructor (public company: string, public location: string, public link: string, public summary: string, public category: string, public image: string) { }
 }
