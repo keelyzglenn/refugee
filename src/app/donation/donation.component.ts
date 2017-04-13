604ec19ce41c9354322443123ad214375362f9b5
Minh Phuong <mphuong@kent.edu>
2017-04-13 16:08:07 -0700

    credit card verification

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterContentInit} from '@angular/core';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { FormsModule }   from '@angular/forms';
 import { Donation } from '../donation.model';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { DonationService } from '../donation.service';
 import { Router } from '@angular/router';
+
 // import { FORM_DIRECTIVES } from '@angular/common';
 // import { RadioControlValueAccessor } from './radio_value_accessor';
 
@@ -23,7 +24,7 @@ export class DonationComponent implements OnInit {
   model;
   donations: FirebaseListObservable<any[]>;
 
-  constructor(private router: Router, private donationService: DonationService) {
+  constructor(private router: Router, private donationService: DonationService, private elementRef:ElementRef) {
     this.model = {
       }
   }
@@ -32,6 +33,18 @@ export class DonationComponent implements OnInit {
     this.donations = this.donationService.getDonations();
   }
 
+  ngAfterViewInit() {
+    var node2 = document.createElement('script');
+      node2.src = "https://cdn.rawgit.com/minhchan11/javascript-files/master/jquery.creditCardValidator.js";
+      node2.type = 'text/javascript';
+      node2.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node2);
+
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "https://cdn.rawgit.com/minhchan11/javascript-files/master/card-document.js";
+  this.elementRef.nativeElement.appendChild(s);
+  }
 
 
 }
