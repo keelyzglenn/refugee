4bbbd71a0a66411884261ba1b8006fde4bd52483
Tammy Dang <dangitstammy@gmail.com>
2017-04-13 11:07:27 -0700

    begin donation amount feature

---
+++ src/app/donation.model.ts
@@ -0,0 +1,3 @@
+export class Donation {
+  public donation: number = 896; 
+}
--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { FormsModule }   from '@angular/forms';
+import { Donation } from '../donation.model';
 
 @Component({
   selector: 'app-donation',
