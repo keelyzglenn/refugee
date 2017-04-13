8ef6f28fdeb17d8b90ae2cca3c0aae84d9d5eb0c
Minh Phuong <mphuong@kent.edu>
2017-04-13 11:38:54 -0700

    add script for credit card

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterContentInit } from '@angular/core';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { FormsModule }   from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-donation',
@@ -11,9 +12,35 @@ import { FormsModule }   from '@angular/forms';
 })
 export class DonationComponent implements OnInit {
 
-  constructor() { }
+  loadScript() {
+         let script1 = document.createElement('script');
+         script1.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js";
+         script1.type = 'text/javascript';
+         script1.charset = 'utf-8';
+         document.getElementsByTagName('head')[0].appendChild(script1);
+         let script2 = document.createElement('script');
+         script2.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.2.3/jquery.payment.min.js";
+         script2.type = 'text/javascript';
+         script2.charset = 'utf-8';
+         document.getElementsByTagName('head')[0].appendChild(script2);
+         let script3 = document.createElement('script');
+         script3.src = "https://js.stripe.com/v2/";
+         script3.type = 'text/javascript';
+         script3.charset = 'utf-8';
+         document.getElementsByTagName('head')[0].appendChild(script3);
+         let script4 = document.createElement('script');
+         script4.src = "https://cdn.rawgit.com/minhchan11/javascript-files/master/donation-script.js";
+         script4.type = 'text/javascript';
+         script4.charset = 'utf-8';
+         document.getElementsByTagName('head')[0].appendChild(script4);
+     }
+  constructor(private elementRef:ElementRef) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.loadScript();
   }
 
 }
