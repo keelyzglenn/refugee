b62910f5c419f693205410e08c9a9cc4c52c0a76
Cassie Musolf and Keely Glenn <cassie+keely@>
2017-04-13 14:57:31 -0700

    add amount donated

---
+++ src/app/donation.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Donation } from './donation.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Injectable()
+export class DonationService {
+  donations: FirebaseListObservable<any[]>;
+
+
+  constructor(private angularFire: AngularFire) {
+    this.donations = angularFire.database.list('donations');
+  }
+
+  getDonations(){
+    return this.donations;
+  }
+}
--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { FormsModule }   from '@angular/forms';
-
+import { Donation } from '../donation.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { DonationService } from '../donation.service';
+import { Router } from '@angular/router';
 // import { FORM_DIRECTIVES } from '@angular/common';
 // import { RadioControlValueAccessor } from './radio_value_accessor';
 
@@ -12,18 +15,23 @@ import { FormsModule }   from '@angular/forms';
   styleUrls: ['./donation.component.css'],
   // directives: [FORM_DIRECTIVES, RadioControlValueAccessor],
   providers: [Ng2Bs3ModalModule,
-            FormsModule]
+            FormsModule,
+          DonationService]
 })
+
 export class DonationComponent implements OnInit {
   model;
+  donations: FirebaseListObservable<any[]>;
 
-  constructor() {
+  constructor(private router: Router, private donationService: DonationService) {
     this.model = {
-
-    }
+      }
   }
 
   ngOnInit() {
+    this.donations = this.donationService.getDonations();
   }
+
+
 
 }
